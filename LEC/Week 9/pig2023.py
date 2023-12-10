933001c92f8f4011a0592e741445bf58c1e66e27
KaranMBT <karanthumar123321@gmail.com>
2023-12-10 15:59:14 -0500

    pig

--- LEC/Week 9/pig2023.py
+++ LEC/Week 9/pig2023.py
@@ -54,3 +54,78 @@ def holdAt20orGoal(limit, score):
         else:
             turnTotal += roll
     return turnTotal, score + turnTotal
+
+#What is the expected number of turns per solitaire game with a hold-at-20-or-goal play policy? Simulate a given number of solitaire Pig games where a player rolls until a 1 (“pig”) is rolled, or the turn total is greater than or equal to 20, or the score plus the turn total is greater than or equal to 100. Report the average number of turns per game. 
+def holdAt20orGoalOutcomes(trials):
+    outcomes = {0: 0}
+    for val in range(20, 26):
+        outcomes[val] = 0
+    for _ in range(trials):
+        score = holdAt20orGoalGame()
+        outcomes[score] += 1
+    for score in outcomes:
+        print(score, outcomes[score] / trials)
+
+
+
+def holdAt20orGoalGame():
+    playerScore = 0
+    computerScore = 0
+    playerTurn = True
+    while playerScore < 100 and computerScore < 100:
+        if playerTurn:
+            turnTotal = 0
+            while True:
+                roll = random.randint(1, 6)
+                print("Roll:", roll)
+                if roll == 1:
+                    turnTotal = 0
+                    print("Turn Total:", turnTotal)
+                    break
+                else:
+                    turnTotal += roll
+                    print("Turn Total:", turnTotal)
+                    print("Player Score:", playerScore)
+                    print("Computer Score:", computerScore)
+                    print()
+                    if playerScore + turnTotal >= 100:
+                        playerScore += turnTotal
+                        print("Player Score:", playerScore)
+                        print("Computer Score:", computerScore)
+                        print("Player wins!")
+                        return
+                    elif turnTotal >= 20:
+                        playerScore += turnTotal
+                        print("Player Score:", playerScore)
+                        print("Computer Score:", computerScore)
+                        print()
+                        break
+            playerTurn = False
+        else:
+            turnTotal = 0
+            while True:
+                roll = random.randint(1, 6)
+                print("Roll:", roll)
+                if roll == 1:
+                    turnTotal = 0
+                    print("Turn Total:", turnTotal)
+                    break
+                else:
+                    turnTotal += roll
+                    print("Turn Total:", turnTotal)
+                    print("Player Score:", playerScore)
+                    print("Computer Score:", computerScore)
+                    print()
+                    if computerScore + turnTotal >= 100:
+                        computerScore += turnTotal
+                        print("Player Score:", playerScore)
+                        print("Computer Score:", computerScore)
+                        print("Computer wins!")
+                        return
+                    elif turnTotal >= 20:
+                        computerScore += turnTotal
+                        print("Player Score:", playerScore)
+                        print("Computer Score:", computerScore)
+                        print()
+                        break
+            playerTurn = True
