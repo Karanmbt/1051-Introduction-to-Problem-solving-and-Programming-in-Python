597c9ce1f3d228342cf26e86fab39ed8ea318083
KaranMBT <karanthumar123321@gmail.com>
2023-12-10 15:56:41 -0500

    same

--- LEC/Week 8/Hawaiian_Pronunciation.py
+++ LEC/Week 8/Hawaiian_Pronunciation.py
@@ -36,7 +36,65 @@ hw = input("Enter a Hawaiian word: ")
 hw = hw.lower()
 
 
-# ask if they want to enter another word
+def convert(word):
+    if word in double_vowels:
+        return double_vowels[word]
+    elif word in Vowels:
+        return Vowels[word]
+    elif word in Consonants:
+        return Consonants[word]
+    elif word in valid_chars:
+        return word
+    else:
+        return ""
+    
+
+def check_valid(word):
+    for char in word:
+        if char not in valid_chars:
+            print(f"{char} is not a valid character")
+            return False
+    return True
+
+
+def check_consonants(word):
+    for char in word:
+        if char in Consonants:
+            print(f"{char} is a consonant")
+            return True
+    return False
+
+
+def check_vowels(word):
+    for char in word:
+        if char in Vowels:
+            print(f"{char} is a vowel")
+            return True
+    return False
+
+
+def check_double_vowels(word):
+    for char in word:
+        if char in double_vowels:
+            print(f"{char} is a double vowel")
+            return True
+    return False
+
+if check_valid(hw):
+    if check_consonants(hw):
+        if check_vowels(hw):
+            if check_double_vowels(hw):
+                for char in hw:
+                    print(convert(char), end="")
+            else:
+                print("No double vowels")
+        else:
+            print("No vowels")
+    else:
+        print("No consonants")
+        
+        
+
 again = input("Would you like to enter another word? (y/n): ")
 while again == "y":
     hw = input("Enter a Hawaiian word: ")
